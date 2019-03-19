7e3c4a11ac8b30446d218d20f898b49727db85f1
Maksim Smirnov <smirn08m@gmail.com>
2019-03-19 22:38:21 +0300

    кнопки, эможи, пустой словарь EPL_CLUB, вынес отдельно токен и прокси

---
+++ settings.py
@@ -0,0 +1,150 @@
+USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
+
+MAIN_KEYS = [    
+                ['Подписка', 'Следующий Матч', 'Информация о последнем матче'],     
+                ['Турнирная таблица', 'Трансляции', 'Об авторе']
+            ]
+
+EPL_CLUBS = [
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+},
+{
+            'name': ' ',
+            'emblem': ' ',
+            'founded': ' ',
+            'stadium': ' ',
+            'web': ' '
+}
+            ]
+
