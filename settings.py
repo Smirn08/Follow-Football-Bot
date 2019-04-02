5e1b80b59580e4b543caa2e60a0daf059ad9bc3d
Maksim Smirnov <smirn08m@gmail.com>
2019-04-02 21:45:39 +0300

    Ввел переменные COLORS и COUNRTY

--- settings.py
+++ settings.py
@@ -6,3 +6,62 @@ MAIN_KEYS = [
                 ['Турнирная таблица'],
                 ['Мой клуб', 'Об авторе']
             ]
+
+COLORS = (
+    {
+        'id': 101,
+        'name': 'red black',
+        'emoji': ':red_circle: :black_circle:'
+        },
+    {
+        'id': 102,
+        'name': 'red white',
+        'emoji': ':red_circle: :white_circle:'
+        },
+    {
+        'id': 103,
+        'name': 'blue white',
+        'emoji': ':large_blue_circle: :white_circle:'
+        },
+    {
+        'id': 104,
+        'name': 'red blue',
+        'emoji': ':red_circle: :large_blue_circle:'
+        },
+    {
+        'id': 105,
+        'name': 'blue yellow',
+        'emoji': ':large_blue_circle: :large_orange_diamond:'
+        },
+    {
+        'id': 106,
+        'name': 'white red black',
+        'emoji': ':white_circle: :red_circle: :black_circle:'
+        },
+    {
+        'id': 107,
+        'name': 'red white black',
+        'emoji': ':red_circle: :white_circle: :black_circle:'
+        },
+    {
+        'id': 108,
+        'name': 'black yellow',
+        'emoji': ':black_circle: :large_orange_diamond:'
+        },
+    {
+        'id': 109,
+        'name': 'black white',
+        'emoji': ':black_circle: :white_circle:'
+        })
+
+COUNTRY = (
+    {
+        'id': 201,
+        'name': 'England',
+        'emoji': ':white_flag:'
+        },
+    {
+        'id': 202,
+        'name': 'Wales',
+        'emoji': ':white_flag:'
+        })
