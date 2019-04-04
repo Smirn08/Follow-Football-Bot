ca75729fe7cd5912d407c6eca4feda771b613147
Maksim Smirnov <smirn08m@gmail.com>
2019-04-04 22:22:45 +0300

    Обновил переменные

--- settings.py
+++ settings.py
@@ -11,57 +11,121 @@ COLORS = (
     {
         'id': 101,
         'name': 'red black',
-        'emoji': ':red_circle: :black_circle:'
+        'emoji': '🔴 ⚫️'
         },
     {
         'id': 102,
         'name': 'red white',
-        'emoji': ':red_circle: :white_circle:'
+        'emoji': '🔴 ⚪️'
         },
     {
         'id': 103,
         'name': 'blue white',
-        'emoji': ':large_blue_circle: :white_circle:'
+        'emoji': '🔵 ⚪️'
         },
     {
         'id': 104,
         'name': 'red blue',
-        'emoji': ':red_circle: :large_blue_circle:'
+        'emoji': '🔴 🔵'
         },
     {
         'id': 105,
         'name': 'blue yellow',
-        'emoji': ':large_blue_circle: :large_orange_diamond:'
+        'emoji': '🔵 💛'
         },
     {
         'id': 106,
         'name': 'white red black',
-        'emoji': ':white_circle: :red_circle: :black_circle:'
+        'emoji': '⚪️ 🔴 ⚫️'
         },
     {
         'id': 107,
         'name': 'red white black',
-        'emoji': ':red_circle: :white_circle: :black_circle:'
+        'emoji': '🔴 ⚪️ ⚫️'
         },
     {
         'id': 108,
         'name': 'black yellow',
-        'emoji': ':black_circle: :large_orange_diamond:'
+        'emoji': '⚫️ 💛'
         },
     {
         'id': 109,
         'name': 'black white',
-        'emoji': ':black_circle: :white_circle:'
+        'emoji': '⚫️ ⚪️'
         })
 
 COUNTRY = (
     {
         'id': 201,
         'name': 'England',
-        'emoji': ':white_flag:'
+        'emoji': '🏴󠁧󠁢󠁥󠁮󠁧󠁿'
         },
     {
         'id': 202,
         'name': 'Wales',
-        'emoji': ':white_flag:'
+        'emoji': '🏴󠁧󠁢󠁷󠁬󠁳󠁿'
         })
+
+CLUB_COMMAND_LIST = [
+        'afcb', 'afc', 'bha', 'bfc', 'car',
+        'cfc', 'cpfc', 'efc', 'ffc', 'htfc',
+        'lei', 'lfc', 'mcfc', 'mufc', 'new',
+        'sou', 'tot', 'wfc', 'whu', 'wol'
+]
+
+LIST_ONE = f'''*AFC Bournemouth*
+/afcb
+*Arsenal FC*
+/afc
+*Brighton & Hove Albion*
+/bha
+*Burnley FC*
+/bfc
+*Cardiff City*
+/car'''
+
+LIST_TWO = f'''*Chelsea FC*
+/cfc
+*Crystal Palace*
+/cpfc
+*Everton FC*
+/efc
+*Fulham FC*
+/ffc
+*Huddersfield Town*
+/htfc'''
+
+LIST_THREE = f'''*Leicester City*
+/lei
+*Liverpool FC*
+/lfc
+*Manchester City*
+/mcfc
+*Manchester United*
+/mufc
+*Newcastle United*
+/new'''
+
+LIST_FOUR = f'''*Southampton FC*
+/sou
+*Tottenham Hotspur*
+/tot
+*Watford FC*
+/wfc
+*West Ham United*
+/whu
+*Wolverhampton Wanderers*
+/wol'''
+
+
+# CLUB_PRINT_TEXT = f'''*{t_info[2]}* {t_info[6]}
+# {t_info[3]} | {t_info[8]}
+
+# *Founded:* {t_info[4]}
+
+# *Home:* {t_info[9]}
+# *Capacity:* {t_info[10]}
+# *Address:* {t_info[11]}
+
+# *Web:* [{t_info[13].replace('http://','')
+#                    .replace('https://','')}]({t_info[13]})'''
